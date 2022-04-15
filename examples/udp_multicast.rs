bc9dc70ea30e5a5a0ed46c5783674e844579d3c9
jasta <jasta@devtcg.org>
2022-04-15 13:34:34 -0700

    Add multicast support, though I am fairly certain the IPv6 branch is not correct.

---
+++ examples/udp_multicast.rs
@@ -0,0 +1,31 @@
+use std::net::SocketAddr;
+
+use coap_lite::link_format::LINK_ATTR_RESOURCE_TYPE;
+use coap_server::app::{CoapError, Request, Response};
+use coap_server::{app, CoapServer, FatalServerError, UdpTransport};
+
+#[tokio::main]
+async fn main() -> Result<(), FatalServerError> {
+    env_logger::init();
+
+    // Try `coap-client -B 5 -N -m get coap://224.0.1.187/.well-known/core`.
+    let transport = UdpTransport::new("0.0.0.0:5683")
+        .enable_multicast();
+    let server = CoapServer::bind(transport).await?;
+    server
+        .serve(
+            app::new()
+                .resource(
+                    app::resource("/hello")
+                        .link_attr(LINK_ATTR_RESOURCE_TYPE, "hello")
+                        .get(handle_get_hello),
+                )
+        )
+        .await
+}
+
+async fn handle_get_hello(request: Request<SocketAddr>) -> Result<Response, CoapError> {
+    let mut response = request.new_response();
+    response.message.payload = b"Hello.".to_vec();
+    Ok(response)
+}
--- src/udp/mod.rs
+++ src/udp/mod.rs
@@ -1,4 +1,5 @@
-use std::net::SocketAddr;
+use std::io;
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr};
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
@@ -6,6 +7,7 @@ use async_trait::async_trait;
 use bytes::BytesMut;
 use coap_lite::Packet;
 use futures::{Sink, Stream};
+use log::debug;
 use pin_project::pin_project;
 use tokio::net::{ToSocketAddrs, UdpSocket};
 use tokio_util::codec::{Decoder, Encoder};
@@ -20,19 +22,59 @@ pub(crate) mod udp_framed_fork;
 pub struct UdpTransport<A: ToSocketAddrs> {
     addresses: A,
     mtu: Option<u32>,
+    multicast: bool,
+    multicast_joiner: MulticastJoiner,
+}
+
+#[derive(Default, Debug, Clone)]
+struct MulticastJoiner {
+    custom_group_joins: Vec<MulticastGroupJoin>,
+    default_ipv4_interface: Option<Ipv4Addr>,
+    default_ipv6_interface: Option<u32>,
+}
+
+#[derive(Debug, Clone)]
+pub enum MulticastGroupJoin {
+    Ipv4(Ipv4Addr, Option<Ipv4Addr>),
+    Ipv6(Ipv6Addr, Option<u32>),
 }
 
 impl<A: ToSocketAddrs> UdpTransport<A> {
     pub fn new(addresses: A) -> Self {
+        let (mtu, multicast, multicast_joiner) =
+            Default::default();
         Self {
             addresses,
-            mtu: None,
+            mtu,
+            multicast,
+            multicast_joiner,
         }
     }
 
     /// Manually set the MTU that will be used for block-wise transfer handling purposes.
-    pub fn set_mtu(&mut self, mtu: u32) {
+    pub fn set_mtu(mut self, mtu: u32) -> Self {
         self.mtu = Some(mtu);
+        self
+    }
+
+    pub fn enable_multicast(mut self) -> Self {
+        self.multicast = true;
+        self
+    }
+
+    pub fn set_multicast_default_ipv4_interface(mut self, interface: Ipv4Addr) -> Self {
+        self.multicast_joiner.default_ipv4_interface = Some(interface);
+        self
+    }
+
+    pub fn set_multicast_default_ipv6_interface(mut self, interface: u32) -> Self {
+        self.multicast_joiner.default_ipv6_interface = Some(interface);
+        self
+    }
+
+    pub fn add_multicast_join(mut self, join: MulticastGroupJoin) -> Self {
+        self.multicast_joiner.custom_group_joins.push(join);
+        self
     }
 }
 
@@ -42,6 +84,9 @@ impl<A: ToSocketAddrs + Sync + Send> Transport for UdpTransport<A> {
 
     async fn bind(self) -> Result<BoxedFramedBinding<Self::Endpoint>, TransportError> {
         let socket = UdpSocket::bind(self.addresses).await?;
+        if self.multicast {
+            self.multicast_joiner.join(&socket)?;
+        }
         let local_addr = socket.local_addr()?;
         let framed_socket = UdpFramed::new(socket, Codec::default());
         let binding = UdpBinding {
@@ -50,6 +95,55 @@ impl<A: ToSocketAddrs + Sync + Send> Transport for UdpTransport<A> {
             mtu: self.mtu,
         };
         Ok(Box::pin(binding))
+    }
+}
+
+impl MulticastJoiner {
+    fn join(self, socket: &UdpSocket) -> io::Result<()> {
+        let ipv4_interface = match socket.local_addr()? {
+            SocketAddr::V4(ipv4) => Some(*ipv4.ip()),
+            _ => None,
+        };
+        let joins = if self.custom_group_joins.is_empty() {
+            self.determine_default_joins(&socket)?
+        } else {
+            self.custom_group_joins
+        };
+
+        for join in joins {
+            debug!("Joining {join:?}...");
+            match join {
+                MulticastGroupJoin::Ipv4(addr, interface) => {
+                    let resolved_interface = interface
+                        .or(self.default_ipv4_interface)
+                        .or(ipv4_interface)
+                        .unwrap();
+                    socket.join_multicast_v4(addr, resolved_interface)?;
+                }
+                MulticastGroupJoin::Ipv6(addr, interface) => {
+                    let resolved_interface = interface
+                        .or(self.default_ipv6_interface)
+                        .unwrap_or(0);
+                    socket.join_multicast_v6(&addr, resolved_interface)?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn determine_default_joins(&self, socket: &UdpSocket) -> io::Result<Vec<MulticastGroupJoin>> {
+        let mut joins = Vec::new();
+        match socket.local_addr()? {
+            SocketAddr::V4(_) => {
+                joins.push(MulticastGroupJoin::Ipv4("224.0.1.187".parse().unwrap(), None));
+            }
+            SocketAddr::V6(_) => {
+                joins.push(MulticastGroupJoin::Ipv6("ff02::fd".parse().unwrap(), None));
+                joins.push(MulticastGroupJoin::Ipv6("ff05::fd".parse().unwrap(), None));
+            }
+        }
+        Ok(joins)
     }
 }
 
